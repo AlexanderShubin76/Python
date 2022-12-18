55942717413d11875ee721be1a22560d13bd30b9
Alexander <pastukh2007@yandex.ru>
2022-12-18 10:18:17 +0300

    homework_1.0

--- Homework_Python/Homework_1/2.py
+++ Homework_Python/Homework_1/2.py
@@ -2,3 +2,12 @@
 #  ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z (расшифровка этого выражения:
 # not (x[0] or x[1] or x[2] = not x[0] and not x[1] and not x[2])
 # для всех значений предикат.
+def check_truth(x,y,z):
+    if not(x or y or z) == (not x and not y and not z):
+        print('True')
+    else:
+        print('False')
+num1 = int(input('Введите число 1: '))
+num2 = int(input('Введите число 2: '))
+num3 = int(input('Введите число 3: '))
+check_truth(num1, num2, num3)
