afe6e8ad3066a0b6fd406a2e3f1908c4044fd571
Alexander <pastukh2007@yandex.ru>
2022-12-19 21:38:27 +0300

    H_2.1

--- Homework_Python/Homework_2/4.py
+++ Homework_Python/Homework_2/4.py
@@ -7,15 +7,16 @@ list_1 = []
 list_2 = []
 n = int(input('Введите значение N > 0: '))
 if (n > 0):
-    for i in range (n):  
+    for i in range(n):
         list_1.append(randint(-n, n))
     print('list_1 =', ''.join(str(list_1)))
-    for i in range (randint(1, n)):
+    for i in range(randint(1, n)):
         list_2.append(randint(0, n-1))
     print('list_2 =', ''.join(str(list_2)))
     result = 1
     for i in list_2:
         result *= list_1[i]
-    print(f'Произведение элементов списка list_1 на позициях списка list_ 2 равно: {result}')
+    print(
+        f'Произведение элементов списка list_1 на позициях списка list_ 2 равно: {result}')
 else:
     print('Ошибка ввода')
--- Homework_Python/Homework_2/5.py
+++ Homework_Python/Homework_2/5.py
@@ -1,11 +1,11 @@
 # 5. Реализуйте алгоритм перемешивания списка.
 from random import randint
 list_1 = []
- 
-for i in range (10):  
+
+for i in range(10):
     list_1.append(randint(-100, 100))
 print('list_1 =', ''.join(str(list_1)))
-for i in range (8):
+for i in range(8):
     (list_1[i], list_1[i+2]) = (list_1[i+2], list_1[i])
- 
+
 print('Вид списка list_1 после перемешивания ', ''.join(str(list_1)))
