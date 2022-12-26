5e2d46c0c70b538afe802f2e2b1281ebfa8a1c07
Alexander <pastukh2007@yandex.ru>
2022-12-26 22:52:14 +0300

    1

---
+++ Seminars/rekur_fact.py
@@ -0,0 +1,15 @@
+# Вычисление факториала с помощью хвостовой рекурсии
+# def Factorials(n, result = 1):
+#     if(n<1):
+#         return result;
+#     return Factorials(n-1, n*result);
+
+# n = int(input('Введите целое число: '));
+
+# listFactorials = [];
+
+# for i in range(1, n+1):
+#     listFactorials.append(Factorials(i));
+
+# print(listFactorials);
+
