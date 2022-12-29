811d91806af30683aa15c7e12fa178b16e839520
Alexander <pastukh2007@yandex.ru>
2022-12-30 00:49:53 +0300

    Homework

--- Homework_Python/Homework_4_/5.py
+++ Homework_Python/Homework_4_/5.py
@@ -1,28 +1,33 @@
 # 5. Даны два файла, в каждом из которых находится запись многочлена.
 # Задача - сформировать файл, содержащий сумму многочленов.
-import get_polynomial as g_p
-k_2 = int(input('Задайте натуральную степень k: '))
-flag = g_p.get_polynomial(k_2)
-# print(flag)
-with open('file(task_5-1).txt', 'w') as data:
-    data.write(flag)
 
-flag_1 = g_p.get_polynomial(k_2)
-# print(flag_1)
-with open('file(task_5-2).txt', 'w') as data:
-    data.write(flag_1)
-f_1 = open('file(task_5-1).txt', 'r')
-for line in f_1:
-    j = line
-    print(j)
-data.close()
+with open('file(task_5-1).txt', 'r') as data:
+    for line in data:
+        p_1 = line
+        print(p_1)
 
-f_2 = open('file(task_5-2).txt', 'r')
-for line1 in f_2:
-    z = line1
-    print(z)
-data.close()
-polynomial_1 = j[:-4]
-polynomial_2 = z[:-4]
-j_list = j.split(' + ')
-z_list = z.split(' + ')
+with open('file(task_5-2).txt', 'r') as data:
+    for line in data:
+        p_2 = line
+        print(p_2)
+
+p_1_list = p_1.split(' + ')
+p_2_list = p_2.split(' + ')
+
+str_3 = str(int(p_1_list[-1]) + int(p_2_list[-1]))
+
+p_1_list_1 = p_1_list[1]
+p_2_list_1 = p_2_list[1]
+str_2 = str(int(p_1_list_1[:-1]) + int(p_2_list_1[:-1])) + 'x'
+
+p_1_list_0 = p_1_list[0]
+p_2_list_0 = p_2_list[0]
+if (p_1_list_0[-1] == p_2_list_0[-1]):
+    str_1 = str(int(p_1_list_0[:-3]) + int(p_2_list_0[:-3])) + p_1_list_0[-3:]
+if (p_1_list_0[-1] != p_2_list_0[-1]):
+    str_1 = f'{p_1_list[0]} + {p_2_list[0]}'
+result = f'{str_1} + {str_2} + {str_3}'
+print(result)
+
+with open('file(task_5-3).txt', 'w') as data:
+    data.write(result)
--- Homework_Python/Homework_4_/get_polynomial.py
+++ Homework_Python/Homework_4_/get_polynomial.py
@@ -11,16 +11,16 @@ def get_polynomial(k):
         result = f'{str_2} + {a+c} = 0'
         return result
     elif (k == 0 and a+c == 0):
-        result = f'x = 0'
+        result = 'x = 0'
         return result
     elif k == 1 and a+b != 0 and c != 0:
         result = f'{a+b}x + {c} = 0'
         return result
     elif k == 1 and (a+b+c) == 0 or (a+b) == 0:
-        result = f'Многочлен не существует'
+        result = 'Многочлен не существует'
         return result
     elif k == 1 and a+b != 0 and c == 0:
-        result = f'x= 0'
+        result = 'x= 0'
         return result
     elif k >= 2 and a != 0 and b != 0 and c != 0:
         result = f'{a}x^{k} + {str_2} + {c} = 0'
@@ -35,13 +35,13 @@ def get_polynomial(k):
         result = f'{a}x^{k} + {str_2} = 0'
         return result
     elif k >= 2 and a == 0 and b == 0 and c != 0:
-        result = f'Многочлен не существует'
+        result = 'Многочлен не существует'
         return result
     elif k >= 2 and a != 0 and b == 0 and c == 0:
-        result = f'x= 0'
+        result = 'x= 0'
         return result
     elif k >= 2 and a == 0 and b != 0 and c == 0:
-        result = f'x = 0'
+        result = 'x = 0'
         return result
     else:
         result = 'Ошибка ввода k'
