5ec8617fb5581a4333eb92d6e163b1d7832d442e
AlexanderShubin76 <115036659+AlexanderShubin76@users.noreply.github.com>
2023-01-19 11:50:26 +0300

    Update 4.py

--- Homework_Python/Homework_5/4.py
+++ Homework_Python/Homework_5/4.py
@@ -1,4 +1,5 @@
-# 4. Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+with open('file_1.txt', 'w') as data:
+    data.write('AAAAAAAAAAAAAAAAAABBBFFFFFGGEEEEEEEEEEEEEEEEEEDGDSS')
 with open('file_1.txt', 'r') as data:
     text = data.read()
 print(text)
@@ -14,8 +15,8 @@ def rle_fun(txt):
                 rle_text += (str(count) + txt[i])
 
             else:
-                count+=1
                 rle_text = rle_text[:-1-len(str(count))]
+                count+=1
                 rle_text += (str(count) + txt[i])
     return rle_text
 rle_txt = rle_fun(text)
@@ -26,25 +27,21 @@ with open('rle_file.txt', 'w') as data_1:
 
 with open('rle_file.txt', 'r') as data_2:
     compressed_text = data_2.read()
-# print(int(compressed_text[:2]) * compressed_text[2])
-# print(compressed_text[:2].isdigit())
+
 def recover_file(rle_str):
     new_string = ''
     y = len(rle_str)
-    for x in range(y):
-        if x!=0:
-            if rle_str[x].isalpha():
-                new_string += (int(rle_str[:x]) * rle_str[x])
-                rle_str = rle_str[x+1:]
-                y -= x+1
-                print(y)
-                x -= x+1
-                print(x)
-
-                print(new_string)
-                print(rle_str)
+    x = 1
+    while( 0<x<y):
+        if rle_str[x].isalpha():
+            new_string += (int(rle_str[:x]) * rle_str[x])
+            rle_str = rle_str[x+1:]
+            y -= x+1
+            x = 1
+        else:
+            x+=1
     return new_string
-yx= recover_file(compressed_text)
+yx = recover_file(compressed_text)
 print(yx)
-# with open('recover_file.txt', 'w') as data_3:
-#     data_3.write(y)
+with open('recover_file.txt', 'w') as data_3:
+    data_3.write(yx)
