a9ddbfba81919f178107a99bbcfbe43cdc66ef58
AlexanderShubin76 <115036659+AlexanderShubin76@users.noreply.github.com>
2023-01-20 16:11:47 +0300

    Update 4.py

--- Homework_Python/Homework_5/4.py
+++ Homework_Python/Homework_5/4.py
@@ -1,24 +1,21 @@
 # 4. Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
 with open('file_1.txt', 'w') as data:
-    data.write('AAAAAAAAAAAAAAAAAABBBFFFFFGGEEEEEEEEEEEEEEEEEEDGDSS')
+    data.write('AAAAAAAAAAAAAAAAAABBBFFFFFFFFFFFFFFFFFFFFFGGEEEEEEEEEEEEEEEEEEDGDS')
 with open('file_1.txt', 'r') as data:
     text = data.read()
 print(text)
 def rle_fun(txt):
     rle_text = ''
     count = 1
-    for i in range(len(txt)):
-        if not i:
-            rle_text = str(count) + txt[0]
+    for i in range(1, len(txt)):
+        if txt[i] == txt [i-1]:
+            count +=1
         else:
-            if txt[i] != txt [i-1]:
-                count = 1
-                rle_text += (str(count) + txt[i])
-
-            else:
-                rle_text = rle_text[:-1-len(str(count))]
-                count+=1
-                rle_text += (str(count) + txt[i])
+            rle_text += (str(count) + txt[i-1])
+            count = 1
+        if i == len(txt) -1:
+            rle_text += (str(count) + txt[i])
+        
     return rle_text
 rle_txt = rle_fun(text)
 print(rle_txt)
