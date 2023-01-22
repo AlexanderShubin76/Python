d4188b5eb7495f7b8ab2b7316581f42d37f1f586
Alexander <pastukh2007@yandex.ru>
2023-01-22 11:02:23 +0300

    H-6_1

--- Homework_Python/Homework_6/5.py
+++ Homework_Python/Homework_6/5.py
@@ -4,13 +4,12 @@
 # - список на основе n, а позиции элементов lst2=[3,1].
 from random import randint
 num = int(input('Задайте длину списка из N элементов, заполненных числами из промежутка [-N, N]: '))
-list_1 = [randint(-num, num) for x in range(num)]
+list_1 = list(set([randint(-num, num) for x in range(num)]))
 print(f'Список №1 чисел будет выглядеть следующим образом: {list_1}')
-list_2 = [randint(0, num-1) for y in range(num)]
+list_2 = list(set([randint(0, len(list_1)-1) for y in range(num)]))
 print(f'Список №2 чисел с позициями будет выглядеть следующим образом: {list_2}')
-list_3 = list(filter(lambda x: list_2.count(x) == 1, list_2))
-print(f'Отсортированный список чисел с позициями будет выглядеть следующим образом: {list_3}')
+
 result = 1
-for i in range(len(list_3)):
-    result *= list_1[list_3[i]]
+for i in range(len(list_2)):
+    result *= list_1[list_2[i]]
 print(f'Произведение элементов списка №1 с позициями указанными в списке №2 будет равно: {result} ')
