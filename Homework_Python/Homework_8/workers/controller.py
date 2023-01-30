5f13561a860f4066bc93df13de043ed13b134d14
AlexanderShubin76 <115036659+AlexanderShubin76@users.noreply.github.com>
2023-01-30 17:06:06 +0300

    Update controller.py

--- Homework_Python/Homework_8/workers/controller.py
+++ Homework_Python/Homework_8/workers/controller.py
@@ -1,23 +1,18 @@
 # Данный модуль отвечает за взаимодействие между модулем model и действиями пользователя, полученными
-# в модуле view. Тут описаны 2 метода. Один для экспорта данных из телефонного справочника
-# "phone_book.p" # в файлы формата .txt или .csv. Другой метод нужен для внесения изменения в телефонный справочник.
+# в модуле view. Тут описан 1 метод для работы со словарем workers.
 
 import model as md
 import view
 
-def create_phone_book(): # Вносим изменения в телефонную книгу
-    for i in md.phone_book.items():
-            print(f'{i}\n')
-    arg = view.view_phone_book()
-    s = 1
-    while s !=0:
-        if arg == 0:
-            return
-
+def operations_with_workers(): # Вносим изменения в телефонную книгу
+    print_workers()
+    arg = view.show_menu()
+    while arg != 9:
+        
         if arg == 1:
-            a,b,c,d,e,i = view.new_id()
-            md.add_id(a,b,c,d,e,i)
-
+            id = get_id()
+            search_worker(id)
+        
         if arg == 2:
             md.del_id(view.remove_id())
         
@@ -25,21 +20,42 @@ def create_phone_book(): # Вносим изменения в телефонну
             j,k,l,m,n,o = view.rerecord_id()
             md.add_id(j,k,l,m,n,o)
 
-        for i in md.phone_book.items():
-            print(f'{i}\n')
+        print_workers()
+        
+        arg = view.show_menu()
 
-        s = view.select_action()
+
+def create_workers(): 
+    print_workers()
+    # arg = view.show_menu()
+    while arg != 9:
+        
+        if arg == 1:
+            id = get_id()
+            search_worker(id)
+        
+        if arg == 2:
+            md.del_id(view.remove_id())
+        
+        if arg == 3:
+            j,k,l,m,n,o = view.rerecord_id()
+            md.add_id(j,k,l,m,n,o)
+
+        print_workers()
+        
+        # arg = view.show_menu()
+        
 
 def export_format(): # Экспортируем данные телефонной книги в нужный нам формат
-    for i in md.phone_book.items():
+    for i in md.workers.items():
         print(f'{i}\n')
         
     n = view.choice_export_format()
     if n == 0: # Не экспортируем данные
         return
     if n == 1:
-        md.export_txt_format('phone_book.txt')
+        md.export_txt_format('workers.txt')
     if n == 2:
-        md.export_csv_format('phone_book.csv')
+        md.export_csv_format('workers.csv')
     
 
