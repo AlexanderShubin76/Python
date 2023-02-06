371efd63bdbb9e6173b8a14dd728cc21b3f19db9
Alexander <pastukh2007@yandex.ru>
2023-02-06 23:09:27 +0300

    ffs

--- Seminars/Seminar_7/5.py
+++ Seminars/Seminar_7/5.py
@@ -2,6 +2,7 @@
 # При решении задачи используйте комбинацию функций filter, map, sum.
 # Обратите внимание: на 9 должно делиться исходное двузначное число, а не его квадрат.
 lst = list(filter(lambda x: not x ** 0.5%9, map(lambda x: x**2, [x for x in range(10,100)])))
-summa = sum(filter(lambda x: not x ** 0.5%9, map(lambda x: x**2, [x for x in range(10,100)])))
+# summa = sum(filter(lambda x: not x ** 0.5%9, map(lambda x: x**2, [x for x in range(10,100)])))
+summa = sum(map(lambda x: x**2, filter(lambda x: x%9 ==0, [x for x in range(10, 100)])))
 print(*lst)
 print(summa)
